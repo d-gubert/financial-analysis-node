d5ff52cd78d5ed91af8d4d89d18a4f9a89118ceb
Douglas Gubert <douglas.gubert@gmail.com>
2015-10-08 00:06:30 -0300

    Experimenting

--- financial-analysis.js
+++ financial-analysis.js
@@ -1,1 +1,23 @@
-export default test = {};
+'use strict'
+
+import ASQ from 'asynquence';
+import fs from 'fs';
+
+console.log(__filename);
+
+ASQ('read-file')
+	.then(done => {
+		let chunks = [], bufferLength = 0;
+		fs.createReadStream(__filename)
+			.on('data', chunk => {
+				chunks.push(chunk);
+				bufferLength += chunk.length;
+			})
+			.on('end', () => {
+				done(Buffer.concat(chunks, bufferLength));
+			})
+	})
+	.then((done, buffer) => {
+		console.log(buffer.toString());
+	});
+
